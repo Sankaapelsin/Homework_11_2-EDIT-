73b3f500f1bf3c2e3ba811224ca345b5bf5219ca
Aleksand Batuev <sankaapelsin@gmail.com>
2024-02-22 11:48:04 +0700

    Function(grow)

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def grow(value):
+    """Функция, которая меняет все буквы строки на заглавные"""
     return value.upper()
 
+
+def grow_first(value):
+    """Функция, которая меняет первую букву строки на заглавную"""
+    return value[0].upper() + value[1:]
